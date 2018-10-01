574967890b8d06c42580ebf90ae563637b7c94d2
Jordan Holmes <jordanstholmes@gmail.com>
2018-10-01 12:27:07 -0700

    Made it through event listeners

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+var Fruit = () => (
+  <li>Fruit COOL!</li>
+);
+
+var Veggies = () => (
+  <li>Veggies YO!</li>
+);
+
+var GroceryListItem = (props) => { 
+
+  var onListItemClick = (event) => {
+    console.log('clicked a list item');
+  };
+
+  return <li onClick={onListItemClick}>{props.item}</li>;
+};
+
+
+var GroceryList = (props) => {
+  return (
+    <ul>
+     {props.listItems.map((item) => <GroceryListItem item={item} />)}
+    </ul>
+  );
+}
+
+var myList = ['tisk', 'task', 'adding more'];
+ReactDOM.render(<GroceryList listItems={myList} />, document.getElementById('app'));
+
 // TODO
